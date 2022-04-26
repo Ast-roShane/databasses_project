687375b2c42aaed3f8914dc033c0a1077281e5af
jeremiah pha <jeremiah.pha@eastern.edu>
2022-04-26 00:00:01 -0400

    "INSERTED DATA TO TABLES AND EDITED SOME TABLES"

--- University_data.sql
+++ University_data.sql
@@ -29,9 +29,9 @@ INSERT INTO departments VALUES ('Chemistry', 'Andrews', '950000');
 --------------------------------------Comp. Sci.--------------------------------
 INSERT INTO faculty VALUES ('10101', '100000', 'Walter', 'Huddell','whuddell@eastern.edu','610-225-5530');
 INSERT INTO faculty VALUES ('10102', '100000', 'Gregory', 'Longo','glongo@eastern.edu','610-225-5531');
-INSERT INTO faculty VALUES ('10103', '100000', 'Nicola', 'McLallen','nmcLallen','610-225-5532');
-INSERT INTO faculty VALUES ('10104', '100000', 'Micheal', 'Morabito','mmorabito','610-225-5533');
-INSERT INTO faculty VALUES ('10105', '100000', 'Brandi', 'Henry','bhenry','610-225-5534');
+INSERT INTO faculty VALUES ('10103', '100000', 'Nicola', 'McLallen','nmcLallen@eastern.edu','610-225-5532');
+INSERT INTO faculty VALUES ('10104', '100000', 'Micheal', 'Morabito','mmorabito@eastern.edu','610-225-5533');
+INSERT INTO faculty VALUES ('10105', '100000', 'Brandi', 'Henry','bhenry@eastern.edu','610-225-5534');
 ---------------------------------------------Biology-----------------------------------------------
 INSERT INTO faculty VALUES ('65000', '900000', 'Maria', 'Fichera','mfichera@eastern.edu','610-341-1460');
 INSERT INTO faculty VALUES ('65001', '900000', 'Wayne', 'Lutz','wlutz@eastern.edu','610-341-1461');
@@ -73,7 +73,7 @@ INSERT INTO students VALUES ('019991', 'Jules','Brandt', 'jules.brandt@eastern.e
 INSERT INTO students VALUES ('023121', 'Juan','Chavez', 'juan.chavez@eastern.edu', ' 231-881-4310');
 INSERT INTO students VALUES ('044552', 'Jah','Peltier', 'jah.peltier@eastern.edu', '215-547-9097');
 INSERT INTO students VALUES ('045678', 'Chance','Levy', 'chance.levy@eastern.edu', ' 505-644-0457');
-INSERT INTO students VALUES ('054321', 'Walter','Williams', 'Comp. Sci.', '505-644-0227');
+INSERT INTO students VALUES ('054321', 'Walter','Williams', 'walter.williams@eastern.edu', '505-644-0227');
 INSERT INTO students VALUES ('055739', 'Jose','Sanchez', 'jose.sanchez@eastern.edu', ' 208-541-4879');
 INSERT INTO students VALUES ('070557', 'Jon','Snow', 'jon.snow@eastern.edu', ' 582-222-1973');
 INSERT INTO students VALUES ('076543','Micheal' ,'Brown', 'micheal.brown@eastern.edu', '202-897-4143');
@@ -85,12 +85,12 @@ INSERT INTO students VALUES ('098988', 'Mike','Tanaka', 'mike.tanaka@eastern.edu
 INSERT INTO employees VALUES ('00128','Micheal','Zhang', 'micheal.zhang@eastern.edu', '202-918-2132','8.50');
 INSERT INTO employees VALUES ('12345','Chance','Levy', 'chance.levy@eastern.edu', ' 505-644-0457','8.50');
 INSERT INTO employees VALUES ('23121','Jon','Snow', 'jon.snow@eastern.edu', ' 582-222-1973','7.75');
-INSERT INTO employees VALUES ('44553', );
-INSERT INTO employees VALUES ('45678');
-INSERT INTO employees VALUES ('76543');
-INSERT INTO employees VALUES ('76653');
-INSERT INTO employees VALUES ('98765');
-INSERT INTO employees VALUES ('98988');
+INSERT INTO employees VALUES ('098988', 'Mike','Tanaka', 'mike.tanaka@eastern.edu', '582-583-3559');
+INSERT INTO employees VALUES ('054321', 'Walter','Williams', 'walter.williams@eastern.edu', '505-644-0227');
+INSERT INTO employees VALUES ('023121', 'Juan','Chavez', 'juan.chavez@eastern.edu', ' 231-881-4310');
+INSERT INTO employees VALUES ('076653', 'Trevor','Aoi', 'trevor.aoi@eastern.edu', '512-415-4114');
+INSERT INTO employees VALUES ('019991', 'Jules','Brandt', 'jules.brandt@eastern.edu', '419-326-0712');
+INSERT INTO employees VALUES ('076543','Micheal' ,'Brown', 'micheal.brown@eastern.edu', '202-897-4143');
 
 
 ---------------------------------------------------------------------------------------
@@ -108,17 +108,17 @@ INSERT INTO driving_details('45565','00128','47148236','2022-04-25 16:00:00','20
 -----------------------------------------------------------------------------------------------------------------
 -----------------------------------------------incoming_shipments------------------------------------------------
 
-INSERT INTO incoming_shipments('272118442670','000128',NULL,'12345','2022-04-22 13:23:44','SHELF');
-INSERT INTO incoming_shipments('272118442870','000128',NULL,'12345','2022-04-22 13:23:44','SHELF');
-INSERT INTO incoming_shipments('272118442970','000128',NULL,'12345','2022-04-22 13:23:44','SHELF');
-INSERT INTO incoming_shipments('272177160739',NULL,'10101','12345','2022-04-22 13:25:00','SHELF');
-INSERT INTO incoming_shipments('272056557375',NULL,'10101','12345','2022-04-22 13:25:00','SHELF');
+INSERT INTO incoming_shipments('272118442670','000128',NULL,'12345','2022-04-22', '13:23:44','SHELF');
+INSERT INTO incoming_shipments('272118442870','000128',NULL,'12345','2022-04-22', '13:23:44','SHELF');
+INSERT INTO incoming_shipments('272118442970','000128',NULL,'12345','2022-04-22', '13:23:44','SHELF');
+INSERT INTO incoming_shipments('272177160739',NULL,'10101','12345','2022-04-22', '13:25:00','SHELF');
+INSERT INTO incoming_shipments('272056557375',NULL,'10101','12345','2022-04-22', '13:25:00','SHELF');
 
-INSERT INTO incoming_shipments('572658749645','019991',NULL,'23121','2022-04-23 12:23:44','SHELF');
-INSERT INTO incoming_shipments('546530021754','023121',NULL,'76543','2022-04-23 12:30:44','SHELF');
-INSERT INTO incoming_shipments('295511407701','000128',NULL,'12345','2022-04-23 13:23:44','SHELF');
-INSERT INTO incoming_shipments('938649320680',NULL,'85001','98765','2022-04-23 13:25:00','SHELF');
-INSERT INTO incoming_shipments('420190873696',NULL,'10101','45678','2022-04-23 13:30:00','SHELF');
+INSERT INTO incoming_shipments('572658749645', '019991', NULL, '23121', '2022-04-23', '12:23:44', 'SHELF');
+INSERT INTO incoming_shipments('546530021754', '023121', NULL, '76543', '2022-04-23', '12:30:44', 'SHELF');
+INSERT INTO incoming_shipments('295511407701', '000128', NULL, '12345', '2022-04-23', '13:23:44', 'SHELF');
+INSERT INTO incoming_shipments('938649320680', NULL, '85001', '98765', '2022-04-23', '13:25:00', 'SHELF');
+INSERT INTO incoming_shipments('420190873696', NULL, '10101', '45678', '2022-04-23', '13:30:00', 'SHELF');
 
 
 
@@ -127,15 +127,59 @@ INSERT INTO incoming_shipments('420190873696',NULL,'10101','45678','2022-04-23 1
 -----------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------outgoing_shipments---------------------------------------------------
 
+--FACULY SENDING OUT SHIPMENTS--
+INSERT INTO outgoing_shipments VALUES ('093107151142', '19482', '076653', '2022-03-12', '12:25:00', '2022-03-14', '03:45:00', '2022-03-11', '1300 Eagle Rd, Wayne PA', 'destinattio addrss', 'FedEx', 'notes');
+INSERT INTO outgoing_shipments VALUES ('591080611321', '23532', '23121', '2022-01-21', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('290047273091', '63467', '23121', '2022-02-01', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('934810918767', '96356', '076653', '2022-09-09', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'USPS', notes)
+INSERT INTO outgoing_shipments VALUES ('252430716344', '26434', '23121', '2022-11-25', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
+INSERT INTO outgoing_shipments VALUES ('751974912258', '76356', '076543', '2022-10-15', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('290432154841', '34523', '076653', '2022-12-19', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'USPS', notes)
+INSERT INTO outgoing_shipments VALUES ('155826406179', '45623', '076543', '2022-02-13', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, "FedEx", notes)
+INSERT INTO outgoing_shipments VALUES ('158546422639', '09234', '076653', '2022-08-23', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'USPS', notes)
+INSERT INTO outgoing_shipments VALUES ('150093498291', '01244', '23121', '2022-11-21', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('866838903606', '52124', '019991', '2022-04-18', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
+INSERT INTO outgoing_shipments VALUES ('760261834350', '09576', '019991', '2022-04-27', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
 
 
-
-
-
-
-
+--STUDENTS SENDING OUT SHIPMENTS--
+INSERT INTO outgoing_shipments VALUES ('034771307962', '65457', '023121', '2022-02-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('113392146087', '87547', '00128',  '2022-09-11', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
+INSERT INTO outgoing_shipments VALUES ('847130429998', '12353', '023121', '2022-12-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('522096780712', '35234', '098988', '2022-12-12', ship_time,'2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('262873198406', '76567', '00128',  '2022-12-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('437492192038', '22412', '098988', '2022-09-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('005006703251', '87588', '023121', '2022-11-21', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
+INSERT INTO outgoing_shipments VALUES ('071078106761', '65498', '098988', '2022-04-03', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
+INSERT INTO outgoing_shipments VALUES ('290079079886', '32543', '00128', '2022-05-03', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
+INSERT INTO outgoing_shipments VALUES ('448071707456', '76543', '00128', '2022-02-02', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
 
 
 
 ------------------------------------------customers------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+--FACULTy CUSTOMERS--
+INSERT INTO customers VALUES ('19482', NULL, '85001', 'William Storm','wstorm@eastern.edu','610-342-1461');
+INSERT INTO customers VALUES ('23532', NULL, '85002', 'Lucas Stromberg','lstromberg@eastern.edu','610-342-1462');
+INSERT INTO customers VALUES ('63467', NULL, '10101', 'Walter Huddell','whuddell@eastern.edu','610-225-5530');
+INSERT INTO customers VALUES ('96356', NULL, '10102', 'Gregory Longo','glongo@eastern.edu','610-225-5531');
+INSERT INTO customers VALUES ('26434', NULL, '10103', 'Nicola McLallen', 'nmcLallen@eastern.edu','610-225-5532');
+INSERT INTO customers VALUES ('76356', NULL, '10104', 'Micheal Morabito', 'mmorabito@eastern.edu','610-225-5533');
+INSERT INTO customers VALUES ('34523', NULL, '10105', 'Brandi Henry', 'bhenry@eastern.edu','610-225-5534');
+INSERT INTO customers VALUES ('45623', NULL, '76000', 'Lorie Thomas','lthomas@eastern.edu','610-342-1470');
+INSERT INTO customers VALUES ('09234', NULL, '76001', 'Mary jones','mjones@eastern.edu','610-342-1471');
+INSERT INTO customers VALUES ('01244', NULL, '36004', 'Jeanne Bundens','jbundens@eastern.edu','610-437-1441');
+INSERT INTO customers VALUES ('52124', NULL, '36005', 'Joseph Karpinski','jkarpinski@eastern.edu','610-437-1442');
+INSERT INTO customers VALUES ('09576', NULL, '36058', 'Jeffrey Lawton','jlawton@eastern.edu','610-437-1446');
+
+--STUDENT CUSTOMERS--
+INSERT INTO customers VALUES ('65457','019991', NULL, 'Jules','Brandt jules.brandt@eastern.edu', '419-326-0712');
+INSERT INTO customers VALUES ('87547','023121', NULL, 'Juan','Chavez juan.chavez@eastern.edu', ' 231-881-4310');
+INSERT INTO customers VALUES ('12353','044552', NULL, 'Jah Peltier', 'jah.peltier@eastern.edu', '215-547-9097');
+INSERT INTO customers VALUES ('35234','045678', NULL, 'Chance Levy', 'chance.levy@eastern.edu', ' 505-644-0457');
+INSERT INTO customers VALUES ('76567','054321', NULL, 'Walter Williams', 'walter.williams@eastern.edu', '505-644-0227');
+INSERT INTO customers VALUES ('22412','055739', NULL, 'Jose Sanchez', 'jose.sanchez@eastern.edu', ' 208-541-4879');
+INSERT INTO customers VALUES ('87588','070557', NULL, 'Jon Snow', 'jon.snow@eastern.edu', ' 582-222-1973');
+INSERT INTO customers VALUES ('65498','076543', NULL, 'Micheal Brown', 'micheal.brown@eastern.edu', '202-897-4143');
+INSERT INTO customers VALUES ('32543','076653', NULL, 'Trevor Aoi', 'trevor.aoi@eastern.edu', '512-415-4114');
+INSERT INTO customers VALUES ('76543','098765', NULL, 'Nick Bourikas', 'nick.bourikas@eastern.edu', '505-712-7572');
