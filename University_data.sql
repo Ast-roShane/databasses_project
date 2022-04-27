9f9d78998f437e1ba28e6f26d57eef3a27ba5585
jeremiah pha <jeremiah.pha@eastern.edu>
2022-04-26 23:46:04 -0400

    ADDED DATA TO TABLES

--- University_data.sql
+++ University_data.sql
@@ -95,16 +95,16 @@ INSERT INTO employees VALUES ('076543','Micheal' ,'Brown', 'micheal.brown@easter
 
 ---------------------------------------------------------------------------------------
 --------------------------------------driving_details---------------------------------------
-INSERT INTO driving_details('45565','00128','47148236','2022-04-22 13:23:44','2022-04-22 15:45:21','0.50');
-INSERT INTO driving_details('45565','00128','47148236','2022-04-22 16:00:00','2022-04-22 16:45:30','0.35');
-INSERT INTO driving_details('10101','12345','37859172','2022-04-23 08:30:00','2022-04-23 10:00:00','1.00');
-INSERT INTO driving_details('45565','00128','47148236','2022-04-23 13:30:44','2022-04-23 15:45:21','0.90');
-INSERT INTO driving_details('45565','00128','47148236','2022-04-23 16:00:00','2022-04-23 16:45:30','0.75');
-INSERT INTO driving_details('76543','23121','35028697','2022-04-24 13:23:44','2022-04-24 15:45:21','0.50');
-INSERT INTO driving_details('76543','23121','35028697','2022-04-24 16:00:00','2022-04-24 16:45:30','0.35');
-INSERT INTO driving_details('45565','00128','47148236','2022-04-25 08:30:00','2022-04-25 10:00:00','1.00');
-INSERT INTO driving_details('10101','12345','37859172','2022-04-25 13:30:44','2022-04-25 15:45:21','0.90');
-INSERT INTO driving_details('45565','00128','47148236','2022-04-25 16:00:00','2022-04-25 16:45:30','0.75');
+INSERT INTO driving_details('45565','00128','47148236','2022-04-22 13:23:44','2022-04-22', '15:45:21','0.50');
+INSERT INTO driving_details('45565','00128','47148236','2022-04-22 16:00:00','2022-04-22', '16:45:30','0.35');
+INSERT INTO driving_details('10101','12345','37859172','2022-04-23 08:30:00','2022-04-23', '10:00:00','1.00');
+INSERT INTO driving_details('45565','00128','47148236','2022-04-23 13:30:44','2022-04-23', '15:45:21','0.90');
+INSERT INTO driving_details('45565','00128','47148236','2022-04-23 16:00:00','2022-04-23', '16:45:30','0.75');
+INSERT INTO driving_details('76543','23121','35028697','2022-04-24 13:23:44','2022-04-24', '15:45:21','0.50');
+INSERT INTO driving_details('76543','23121','35028697','2022-04-24 16:00:00','2022-04-24', '16:45:30','0.35');
+INSERT INTO driving_details('45565','00128','47148236','2022-04-25 08:30:00','2022-04-25', '10:00:00','1.00');
+INSERT INTO driving_details('10101','12345','37859172','2022-04-25 13:30:44','2022-04-25', '15:45:21','0.90');
+INSERT INTO driving_details('45565','00128','47148236','2022-04-25 16:00:00','2022-04-25', '16:45:30','0.75');
 -----------------------------------------------------------------------------------------------------------------
 -----------------------------------------------incoming_shipments------------------------------------------------
 
@@ -128,31 +128,30 @@ INSERT INTO incoming_shipments('420190873696', NULL, '10101', '45678', '2022-04-
 ---------------------------------------------------outgoing_shipments---------------------------------------------------
 
 --FACULY SENDING OUT SHIPMENTS--
-INSERT INTO outgoing_shipments VALUES ('093107151142', '19482', '076653', '2022-03-12', '12:25:00', '2022-03-14', '03:45:00', '2022-03-11', '1300 Eagle Rd, Wayne PA', 'destinattio addrss', 'FedEx', 'notes');
-INSERT INTO outgoing_shipments VALUES ('591080611321', '23532', '23121', '2022-01-21', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('290047273091', '63467', '23121', '2022-02-01', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('934810918767', '96356', '076653', '2022-09-09', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'USPS', notes)
-INSERT INTO outgoing_shipments VALUES ('252430716344', '26434', '23121', '2022-11-25', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-INSERT INTO outgoing_shipments VALUES ('751974912258', '76356', '076543', '2022-10-15', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('290432154841', '34523', '076653', '2022-12-19', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'USPS', notes)
-INSERT INTO outgoing_shipments VALUES ('155826406179', '45623', '076543', '2022-02-13', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, "FedEx", notes)
-INSERT INTO outgoing_shipments VALUES ('158546422639', '09234', '076653', '2022-08-23', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'USPS', notes)
-INSERT INTO outgoing_shipments VALUES ('150093498291', '01244', '23121', '2022-11-21', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('866838903606', '52124', '019991', '2022-04-18', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-INSERT INTO outgoing_shipments VALUES ('760261834350', '09576', '019991', '2022-04-27', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-
+INSERT INTO outgoing_shipments VALUES ('093107151142', '19482', '076653', '2022-03-12', '12:25:00', '2022-03-14', '15:45:00', '2022-03-11', '1300 Eagle Rd, Wayne PA 19087', 'destinattio addrss', 'FedEx', 'SHELF');
+INSERT INTO outgoing_shipments VALUES ('591080611321', '23532', '23121', '2022-01-21', '12:30:00', '2022-01-27', '15:45:00', '12:30:00', '1300 Eagle Rd, Wayne PA 19087', '3997 Pearl St Bridgman, Michigan(MI), 49106', 'UPS', 'SHELF')
+INSERT INTO outgoing_shipments VALUES ('290047273091', '63467', '23121', '2022-02-01', '11:25:00', '2022-02-08', '15:30:00', '11:25:00', '1300 Eagle Rd, Wayne PA 19087', '193 Trammell St Dubach, Louisiana(LA), 71235','UPS', 'Envelope')
+INSERT INTO outgoing_shipments VALUES ('934810918767', '96356', '076653', '2022-09-09', '13:30:00', '2022-09-12', '14:30:00', '13:30:00', '1300 Eagle Rd, Wayne PA 19087', '308 Quail Run Bradyville, Tennessee(TN), 37026', 'USPS', 'Package')
+INSERT INTO outgoing_shipments VALUES ('252430716344', '26434', '23121', '2022-11-25', '16:30:00', '2022-11-28', '15:42:00', '16:30:00', '1300 Eagle Rd, Wayne PA 19087', '2857 W 98th Pl Evergreen Park, Illinois(IL), 60805', 'FedEx', 'Package')
+INSERT INTO outgoing_shipments VALUES ('751974912258', '76356', '076543', '2022-10-15', '15:00:00', '2022-10-20', '15:04:00', '15:00:00', '1300 Eagle Rd, Wayne PA 19087', '4855 Hustisford, Wisconsin(WI), 53034', 'UPS', 'Package')
+INSERT INTO outgoing_shipments VALUES ('290432154841', '34523', '076653', '2022-12-19', '15:30:00', '2022-12-30', '12:25:00', '15:30:00', '1300 Eagle Rd, Wayne PA 19087', '946 Sheridan Rd Winthrop Harbor, Illinois(IL), 60096', 'USPS', 'Envelope')
+INSERT INTO outgoing_shipments VALUES ('155826406179', '45623', '076543', '2022-02-13', '15:33:00', '2022-02-14', '15:55:00', '15:33:00', '1300 Eagle Rd, Wayne PA 19087', '5100 57th Ave Bladensburg, Maryland(MD), 20710', "FedEx", 'SHELF')
+INSERT INTO outgoing_shipments VALUES ('158546422639', '09234', '076653', '2022-08-23', '12:22:00', '2022-09-01', '13:30:00', '12:22:00', '1300 Eagle Rd, Wayne PA 19087', '5021 Fillmore Ave Alexandria, Virginia(VA), 22311', 'USPS', 'SHELF')
+INSERT INTO outgoing_shipments VALUES ('150093498291', '01244', '23121', '2022-11-21', '11:21:00', '2022-11-24', '15:30:00', '11:21:00', '1300 Eagle Rd, Wayne PA 19087', '385 Culvers Ln New Haven, Kentucky(KY), 40051', 'UPS', 'SHELF')
+INSERT INTO outgoing_shipments VALUES ('866838903606', '52124', '019991', '2022-04-18', '17:00:00', '2022-04-23', '15:45:00', '17:00:00', '1300 Eagle Rd, Wayne PA 19087', '14650 State Rte #53 W Upper Sandusky, Ohio(OH), 43351', 'FedEx', 'Envelope')
+INSERT INTO outgoing_shipments VALUES ('760261834350', '09576', '019991', '2022-04-27', '14:59:00', '2022-04-28', '15:45:00', '14:59:00', '1300 Eagle Rd, Wayne PA 19087', '18 Albertson Way Bristol, Connecticut(CT), 06010', 'FedEx', 'Envelope')
 
 --STUDENTS SENDING OUT SHIPMENTS--
-INSERT INTO outgoing_shipments VALUES ('034771307962', '65457', '023121', '2022-02-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('113392146087', '87547', '00128',  '2022-09-11', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-INSERT INTO outgoing_shipments VALUES ('847130429998', '12353', '023121', '2022-12-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('522096780712', '35234', '098988', '2022-12-12', ship_time,'2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('262873198406', '76567', '00128',  '2022-12-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('437492192038', '22412', '098988', '2022-09-12', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
-INSERT INTO outgoing_shipments VALUES ('005006703251', '87588', '023121', '2022-11-21', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-INSERT INTO outgoing_shipments VALUES ('071078106761', '65498', '098988', '2022-04-03', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-INSERT INTO outgoing_shipments VALUES ('290079079886', '32543', '00128', '2022-05-03', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'FedEx', notes)
-INSERT INTO outgoing_shipments VALUES ('448071707456', '76543', '00128', '2022-02-02', ship_time, '2022-03-12', delivery_time, paid_date, orgin_address, destination_address, 'UPS', notes)
+INSERT INTO outgoing_shipments VALUES ('034771307962', '65457', '023121', '2022-02-12', '13:29:00', '2022-02-14', '15:32:00', '13:29:00', '1300 Eagle Rd, Wayne PA 19087', '337 Pecan St Levelland, Texas(TX), 79336', 'UPS', 'SHELF')
+INSERT INTO outgoing_shipments VALUES ('113392146087', '87547', '00128',  '2022-09-11', '11:52:00', '2022-09-18', '14:59:00', '11:52:00', '1300 Eagle Rd, Wayne PA 19087', '8363 80th Hwy Ruston, Louisiana(LA), 71270', 'FedEx', 'Package')
+INSERT INTO outgoing_shipments VALUES ('847130429998', '12353', '023121', '2022-12-12', '15:22:00', '2022-12-21', '15:15:00', '15:22:00', '1300 Eagle Rd, Wayne PA 19087', '711 Bedford Ln New Castle, Delaware(DE), 19720', 'UPS', 'Package')
+INSERT INTO outgoing_shipments VALUES ('522096780712', '35234', '098988', '2022-12-12', '12:43:00','2022-12-23', '15:12:00', '12:43:00', '1300 Eagle Rd, Wayne PA 19087', '348 Silver Lake Dr Ocracoke, North Carolina(NC), 27960', 'UPS', 'Package')
+INSERT INTO outgoing_shipments VALUES ('262873198406', '76567', '00128',  '2022-12-12', '10:30:00', '2022-12-14', '13:30:00', '10:30:00', '1300 Eagle Rd, Wayne PA 19087', '6097 67th Hwy Slaughter, Louisiana(LA), 70777', 'UPS', 'Package')
+INSERT INTO outgoing_shipments VALUES ('437492192038', '22412', '098988', '2022-09-12', '15:20:00', '2022-09-23', '13:39:00', '15:20:00', '1300 Eagle Rd, Wayne PA 19087', '1525 S Plummer Ave Chanute, Kansas(KS), 66720', 'UPS', 'Envelope')
+INSERT INTO outgoing_shipments VALUES ('005006703251', '87588', '023121', '2022-11-21', '14:24;00', '2022-12-05', '15:50:00', '2022-11-21', '1300 Eagle Rd, Wayne PA 19087', '684 E 189th St Scranton, Kansas(KS), 66537', 'FedEx', 'Envelope')
+INSERT INTO outgoing_shipments VALUES ('071078106761', '65498', '098988', '2022-04-03', '12:22:00', '2022-04-12', '15:30:00', '2022-04-03', '1300 Eagle Rd, Wayne PA 19087', '1050 Old Us 119 Hwy S Homer City, Pennsylvania(PA), 15748', 'FedEx', 'Package')
+INSERT INTO outgoing_shipments VALUES ('290079079886', '32543', '00128', '2022-05-03', '9:50:00', '2022-05-11', '14:30:00', '2022-05-03', '1300 Eagle Rd, Wayne PA 19087', '217 Winesap Ct Janesville, Wisconsin(WI), 53548', 'FedEx', 'SHELF')
+INSERT INTO outgoing_shipments VALUES ('448071707456', '76543', '00128', '2022-02-02', '10:02:00', '2022-03-01', '14:00:00', '2022-02-02', '1300 Eagle Rd, Wayne PA 19087', '101 Tipperary Dr Phoenixville, Pennsylvania(PA), 19460', 'UPS', 'Envelope')
 
 
 
