5a01b1c5eb970829992913f88195fc512704bd13
astroshane <roshane.bent@eastern.edu>
2022-05-02 16:39:40 -0400

    .

--- Mail_queries.sql
+++ Mail_queries.sql
@@ -8,3 +8,9 @@
 -----------------query3----------------------
 SELECT * origin_address, destination_address
 FROM outgoing_shipments
+
+
+---------------------query8-------------------
+SELECT cust_id,cust_name
+FROM customers
+WHERE student_id and faculty_id is NULL
