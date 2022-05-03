c7aac6995c2c40e219e6717fe3e48ca2df51f27f
astroshane <roshane.bent@eastern.edu>
2022-05-03 17:05:59 -0400

    updated querys

--- Mail_queries.sql
+++ Mail_queries.sql
@@ -36,8 +36,13 @@ how many did he recieve each day and at what times did the packages arrive?*/
 
 
 -----------------query3----------------------
-  SELECT * origin_address, destination_address
-  FROM outgoing_shipments
+/*
+ Find the ID and name of each student who has never received a package at the university.
+*/
+----THIS QUERY LISTS THE NAMES OF STUDENTS WHO HAS NOT RECIEVED A PACKAGE AT THE UNIVERSITY
+  SELECT student_id, first_name,last_name
+  FROM students NATURAL LEFT OUTER JOIN incoming_shipments
+  WHERE student_id IS NULL
 
 
 -----------------query4----------------------
@@ -52,12 +57,26 @@ Add a new employee to the database with the employee values and then output the 
   ORDER BY first_name ASC;
 
 
-  ---------------------query8------------------
-  SELECT cust_id,cust_name
-  FROM customers
-  WHERE student_id and faculty_id is NULL
+  ---------------------query6------------------
+  /*The university has the budget to increase the hourly rate for employees at the mail center.
+  Employees getting paid less than $8 would be considered underpaid. So they will recieve a raise a 15% raise and employees who are getting over 8 will recieve a 5% raise.*/
+  ----THIS QUERY HIGHLIGHTS ALL EMPLOYEES GETTING PAID UNDER $8 AS UNDERPAID AND GIVES THE EMPLOYEES A 15% RAISE. ANY EMPLOYEE GETTING PAID MORE THAN 8 WILL RECIEVE A 5% RAISE.
+  SELECT employee_id,first_name,last_name
+      CASE
+        WHEN hourly_rate<8.00 THEN 'UNDERPAID'
+      END AS pay_status
+    FROM employees
 
---------------------query6-------------------
-  SELECT *
-  FROM faculty
-  WHERE
+
+  UPDATE employees
+  SET hourly_rate =  CASE
+                        WHEN pay_status='UNDERPAID' THEN hourly_rate*1.05
+                        ELSE hourly_rate*1.15
+                      END;
+
+
+--------------------query8-------------------
+
+  SELECT cust_name,email,cust_contact_numbers
+  FROM customers NATURAL LEFT OUTER JOIN faculty NATURAL LEFT OUTER JOIN students
+  WHERE student_id,faculty_id IS NULL;
