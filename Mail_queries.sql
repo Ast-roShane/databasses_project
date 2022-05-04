49057acd9500336df43d6629479bf75082ffbcd4
astroshane <roshane.bent@eastern.edu>
2022-05-03 22:59:26 -0400

    added functions

--- Mail_queries.sql
+++ Mail_queries.sql
@@ -49,7 +49,7 @@ how many did he recieve each day and at what times did the packages arrive?*/
 		FROM customers
 		WHERE customers.student_id IS NULL)AS bar);
 --  WHERE student_id IS NULL
-  
+
 */
 
 -----------------query4----------------------
@@ -67,15 +67,15 @@ Add a new employee to the database with the employee values and then output the 
   ---------------------query6------------------
   /*The university has the budget to increase the hourly rate for employees at the mail center.
   Employees getting paid less than $12 would be considered underpaid. So they will recieve a raise a 15% raise and employees who are getting over 8 will recieve a 5% raise.*/
-  
+
   ----THIS QUERY HIGHLIGHTS ALL EMPLOYEES GETTING PAID UNDER $12 AS UNDERPAID AND GIVES THE EMPLOYEES A 15% RAISE. ANY EMPLOYEE GETTING PAID MORE THAN 8 WILL RECIEVE A 5% RAISE.
-  SELECT employees_id,first_name,last_name, 
+  SELECT employees_id,first_name,last_name,
       CASE
 	 	WHEN hourly_rate< 12.00 THEN 'UNDERPAID'
       END AS pay_status
 	  FROM employees;
 	--------------------once ran run this----------------
-	
+
 	UPDATE employees
   	SET hourly_rate = CASE
                        		WHEN hourly_rate<10.00 THEN hourly_rate*1.05
@@ -88,10 +88,89 @@ Add a new employee to the database with the employee values and then output the 
   SELECT cust_name,email,cust_contact_numbers
   FROM customers NATURAL LEFT OUTER JOIN students
   WHERE faculty_id IS NULL AND student_id IS NULL
-  
+
   -------------------------------------------------
   --------------------------query9------------------------------------
   -------NAME ALL THE EMPLOYEES THAT ROUTED PACKAGES ON APRIL 23 2022-----
   SELECT first_name,last_name
   FROM employees JOIN  incoming_shipments ON employees.employees_id = incoming_shipments.employees_id
   WHERE date_arrived = '2022-04-23'
+
+
+  -----------------------------------------------------------
+
+  CREATE OR REPLACE scan speed(packages VARCHAR(20))
+  RETURNS INTEGER
+  LANGUAGE plpgsql
+  AS
+  $$
+      -- Variable Declarations
+      DECLARE
+        d_count INTEGER;
+      -- SQL Statements
+      BEGIN
+        <...>
+      END;
+  $$;
+
+
+  CREATE OR REPLACE FUNCTION instructor_of(dept_name VARCHAR(20))
+  RETURNS TABLE (IDD VARCHAR(5),
+                namee VARCHAR(20),
+                dept_namee VARCHAR(20),
+                salaryy NUMERIC(8,2))
+
+  LANGUAGE plpgsql
+  AS
+  $$
+      -- SQL Statements
+      BEGIN
+        RETURN QUERY
+        SELECT ID, name, instructor.dept_name, salaryy
+        FROM instructor
+        WHERE instructor.dept_name=instructor_of.dept_name;
+      END;
+  $$;
+
+
+----A function that calculates how many packages a student recieved in a DAY
+
+CREATE OR REPLACE FUNCTION packages_recieved(s_id INTEGER, datee DATE)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE num_packages INTEGER;
+
+
+
+      BEGIN
+          SELECT COUNT(*) INTO num_packages
+          FROM incoming_shipments
+          WHERE student_id = s_id AND datee = date_arrived;
+
+          RETURN num_packages;
+      END;
+$$;
+
+SELECT packages_recieved(000128,'2022-04-22')
+    -----------------------
+CREATE OR REPLACE FUNCTION packages_recieved(s_id INTEGER)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE num_packages INTEGER;
+    DECLARE datee         DATE;
+
+
+      BEGIN
+
+          SELECT EXTRACT(DATE FROM NOW()) INTO datee;
+          SELECT COUNT(*) INTO num_packages
+          FROM incoming_shipments
+          WHERE student_id = s_id AND datee = date_arrived;
+
+          RETURN num_packages;
+      END;
+$$;
